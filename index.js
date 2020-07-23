5980c08c9b9b06dbc756764890e73ebd01602b46
Ciera Hernandez <ciera.hernandez@outlook.com>
2020-07-23 00:48:45 +0000

    Fixed a quotation issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $('.shopping-list').on('click', '.shopping-item-toggle', function() {
   $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
 });
 
-  $('#js-shopping-list-form').on('submit', function(even) {
+  $('#js-shopping-list-form').on('submit', function(event) {
     event.preventDefault();
     var listItem = $('#shopping-list-entry').val();
 
@@ -14,7 +14,7 @@ $('.shopping-list').on('click', '.shopping-item-toggle', function() {
         <button class = 'shopping-item-toggle'>
           <span class = 'button-label'>check</span>
         </button>
-        <button class = 'shopping-item-delete">
+        <button class = 'shopping-item-delete'>
           <span class = 'button-label'>delete</span>
         </button>
       </div>
